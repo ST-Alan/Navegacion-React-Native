a4125b8bc3d4ae47615abf501ad9ba0c86d7ce59
Alan FERMIN <afermin@atc.local>
2023-07-12 16:02:15 -0500

    NagacionPaginas

---
+++ src/theme/appTheme.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    globalMargin:{
+        marginHorizontal:20
+    },
+    title:{
+        fontSize:45,
+        textAlign:'center',
+        marginBottom: 40
+    },
+    botonGrande:{
+        width:100,
+        height:100,
+        backgroundColor:'#552555',
+        borderRadius:20,
+        alignItems:'center',
+        justifyContent:'center',
+        marginRight:10,
+        marginTop: 10
+    },
+    botonGrandeTexto:{
+        color:'white'
+    }
+});
