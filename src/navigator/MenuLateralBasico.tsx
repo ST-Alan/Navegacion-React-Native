a54ac311da2cb80f67ec450dc46858251d5de89b
ST-Alan <contacto@alanfermin.com>
2023-07-26 10:02:03 -0500

    Drawer finalizado

--- src/navigator/MenuLateralBasico.tsx
+++ src/navigator/MenuLateralBasico.tsx
@@ -4,14 +4,25 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import { StackNavigator } from './StackNavigator';
 import { SettingsScreen } from '../screens/SettingsScreen';
+import { useWindowDimensions } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const MenuLateralBasico= ()=> {
+
+  const dimensions = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        drawerPosition: 'right',
+        // drawerType:'slide',
+        drawerType: dimensions.width >= 768 ? 'permanent' : 'slide',
+      }}
+    
+    >
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
-      <Drawer.Screen name="Article" component={SettingsScreen} />
+      <Drawer.Screen name="Article" options={{title: 'Alan'}} component={SettingsScreen} />
     </Drawer.Navigator>
   );
 }
